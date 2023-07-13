d554c08fad3873d62a61647a8dbcb12893e02f9d
guidoceccherini <guido.ceccherini@ec.europa.eu>
2023-07-13 18:19:11 +0200

    new scripts

--- Rscipts/PlotHRL.R
+++ Rscipts/PlotHRL.R
@@ -17,24 +17,38 @@ library(MetBrewer)
 
 
 
-a <- st_read('GEDI_GLOBAL/Hexcell500Filter.shp')
+a <- st_read('GRIDS/GridHex05Deg.shp')
 
-Count19 <- readr::read_csv('GEDI_GLOBAL/TreeCoverHRL.csv')%>% rename(TC18=b1)
+Count19 <- readr::read_csv('Data/TreeCoverHRL05.csv')%>% rename(TC18=b1)
 
-aJ <- a %>% left_join(Count19, by ='FID')%>% filter(TC18>0)%>% drop_na(TC18)
+aJ <- a %>% left_join(Count19, by ='id')%>% filter(TC18>0)%>% drop_na(TC18)
 
 
 st_crs(aJ) = 4326
 
-
-
 plot(aJ['TC18'],breaks = "quantile")
+
+a2 <- st_read('GRIDS/GridHex1Deg.shp')
+
+Count192 <- readr::read_csv('Data/TreeCoverHRL1.csv')%>% rename(TC18=b1)
+
+aJ2 <- a2 %>% left_join(Count192, by ='id')%>% filter(TC18>0)%>% drop_na(TC18)
+
+
+st_crs(aJ2) = 4326
+
+
+
+
 # world <- st_make_valid(world)
 
 # st_write(world,'GEDI_GLOBAL/World.gpkg')
 
 spdf_SL <- ne_countries(country = 'Slovenia',returnclass='sf',scale= 'large')
 # write_sf(spdf_SL, 'SloveniaBorders.shp')
+
+
+
 
 
 europe <- ne_countries(continent = 'europe',returnclass='sf',scale= 'large')
@@ -111,57 +125,21 @@ europe.clipped <- suppressWarnings(st_intersection(europe, st_sfc(europe.bbox, c
 
 
 
-# 2019
-
-Count19 <- readr::read_csv('GEDI_GLOBAL/heights_2019_C_ALL.csv')%>% rename(Tot19=rh98 )
-Count19T <- readr::read_csv('GEDI_GLOBAL/heights_2019_Cgt5_ALL.csv')%>% rename(Trees = rh98)
-
-
-
-aJ2 <- aJ %>% left_join(Count19, by ='FID')
-aJ2 <- aJ2 %>% left_join(Count19T, by ='FID')
-
-
-
-plot(aJ2['Tot19'])
-
-plot(aJ2['Trees'])
-
-aJ2 <- aJ2 %>% mutate(Ratio19 = Trees/Tot19)%>%dplyr::filter(Tot19 > 100000)
-
-
-aJ2 <- aJ2 %>% mutate(DeltaTC = Ratio19 - TC18)%>%dplyr::filter(Tot19 > 100000)
-
-
-plot(aJ2['Ratio19'])
 ggplot() +
   geom_sf(data = europe.clipped) +
-  geom_sf(data= aJ2,aes(fill=Ratio19), alpha = 0.5)+
+  geom_sf(data= aJ,aes(fill=TC18*100), alpha = 0.5)+
   # coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
-  scale_fill_viridis_b(limits=c(0,1)) +
-  ggtitle("Tree Cover EU GEDI")
-ggsave("EU_GEDI.png", width = 25, height = 18, units = "cm",dpi=300,path = 'GEDI_GLOBAL/figures' )
-
+  scale_fill_viridis_c(limits=c(0,100)) +
+  ggtitle("Percentage Forest Cover EU HRL Copernicus")
+ggsave("EU_HRL.png", width = 25, height = 18, units = "cm",dpi=300,path = 'Figures' )
 
 
 ggplot() +
   geom_sf(data = europe.clipped) +
-  geom_sf(data= aJ2,aes(fill=TC18), alpha = 0.5)+
+  geom_sf(data= aJ2,aes(fill=TC18*100), alpha = 0.5)+
   # coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
-  scale_fill_viridis_b(limits=c(0,1)) +
-  ggtitle("Tree Cover EU HRL Copernicus")
-ggsave("EU_HRL.png", width = 25, height = 18, units = "cm",dpi=300,path = 'GEDI_GLOBAL/figures' )
+  scale_fill_viridis_c(limits=c(0,100)) +
+  ggtitle("Percentage Forest Cover EU HRL Copernicus")
+ggsave("EU_HRL2.png", width = 25, height = 18, units = "cm",dpi=300,path = 'Figures' )
 
 
-
-
-ggplot() +
-  geom_sf(data = europe.clipped) +
-  geom_sf(data= aJ2,aes(fill=DeltaTC), alpha = 0.5)+
-  # coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
-  # scale_fill_viridis_b(limits=c(0,1)) +
-  scale_fill_gradient2(limits=c(-0.2, 0.2), oob=squish)+#limits=c(-0.015, 0.015), oob=squish) +
-    ggtitle("Delta Tree Cover GEDI vs. EU HRL Copernicus")
-ggsave("EU_DeltaHRL.png", width = 25, height = 18, units = "cm",dpi=300,path = 'GEDI_GLOBAL/figures' )
-
-mean(aJ2$DeltaTC)
--- Rscipts/PlotsGlobal.R
+++ Rscipts/PlotsGlobal.R
@@ -8,54 +8,24 @@ library(ggplot2)
 library("stringr")
 library(scales)
 
-a <- st_read('GEDI_GLOBAL/Hexcell500Filter.shp')
+a <- st_read('GRIDS/GridHex05Deg.shp')
 
 st_crs(a) = 4326
 
 
-
-plot(a['FID'],breaks = "quantile")
-# world <- st_make_valid(world)
-
-# st_write(world,'GEDI_GLOBAL/World.gpkg')
-
-world <- st_read('GEDI_GLOBAL/World.gpkg')
-world <- st_cast(world, "MULTIPOLYGON")
-
-# 
-# geol_cropped <- st_crop(geol, xmin = 138, xmax = 155, ymin = -32, ymax = -43)
-# world.sf  <- world%>% 
-#   st_intersection(aJTot)
-
 library(ggspatial)
 library(colorblindcheck)
 library(rcartocolor)
-# 
-# ggplot() +
-#   annotation_map_tile(type = "osmgrayscale") +
-#   # annotation_map_tile(type = "stamenbw") +
-#   # annotation_map_tile(type = "cartolight") +
-#   geom_sf(data= a,aes(fill=FID), alpha = 0.5)+
-#   scale_fill_gradient2(limits=c(-0.015, 0.015), oob=squish) +
-#   ggtitle("Hexagons")
-
-ggplot() +
-  geom_sf(data = world) +
-  geom_sf(data= a,aes(fill=FID), alpha = 0.5)+
-  coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
-  # scale_fill_gradient2(limits=c(-0.015, 0.015), oob=squish) +
-  ggtitle("Hexagons")
-ggsave("GEDIH.png", width = 25, height = 18, units = "cm",dpi=300,path = 'GEDI_GLOBAL/figures' )
 
 
 
 
 
 
-Count19 <- readr::read_csv('GEDI_GLOBAL/heights_2019_C.csv')%>% rename(Tot19=rh98 )
-Count19T <- readr::read_csv('GEDI_GLOBAL/heights_2019_Cgt5.csv')%>% rename(Trees = rh98)
-Count19f <- readr::read_csv('GEDI_GLOBAL/heights_2019_Cf.csv')%>% rename(Count19 = rh98)
-Count19TH <- readr::read_csv('GEDI_GLOBAL/heights_2019_Median.csv')%>% rename(THM19 = rh98)
+Count19 <- readr::read_csv('Data//heights_2019_C.csv')%>% rename(Tot19=rh98 )
+Count19T <- readr::read_csv('Data/heights_2019_Cgt5.csv')%>% rename(Trees = rh98)
+Count19f <- readr::read_csv('Data/heights_2019_Cf.csv')%>% rename(Count19 = rh98)
+Count19TH <- readr::read_csv('Data/heights_2019_Median.csv')%>% rename(THM19 = rh98)
 
 Count19f$Count19 <- str_replace(Count19f$Count19, "\\{", "")
 Count19f$Count19 <- str_replace(Count19f$Count19, "\\}", "")
@@ -70,12 +40,17 @@ Count19f1 <- Count19f %>%
   separate(Count19, into = c("vars1", "vars2"), sep="\\s*=\\s*") %>% 
   pivot_wider(names_from = vars1, values_from = vars2, 
               values_fill = list(vars2 = '')) %>%
-  select(-rn)%>%
-  select(-'_count')%>%
-  select(-'null')%>%
-  select(-`system:index`)%>%
-  select(-`.geo`)%>%
-  select(-'0.0')%>%
+  dplyr::select(-rn)%>%
+  # select(-'_count')%>%
+  dplyr::select(-'null')%>%
+  dplyr::select(-`system:index`)%>%
+  dplyr::select(-`.geo`)%>%
+  dplyr::select(-'0.0')%>%
+  dplyr::select(-left)%>%
+  dplyr::select(-right)%>%
+  dplyr::select(-bottom)%>%
+  dplyr::select(-top)%>%
+  
   # rename('0' = `0.0`)%>%
   rename('5' = `1.0`)%>%
   rename('15' = `2.0`)%>%
@@ -121,10 +96,10 @@ p2<-ggplot(data=Count19f1Tot_long, aes(x=H, y=values)) +
   geom_density(stat="identity", fill=NA) 
 p2
 
-aJ <- a %>% left_join(Count19, by ='FID')
-aJ <- aJ %>% left_join(Count19T, by ='FID')
-aJ <- aJ %>% left_join(Count19TH, by ='FID')
-aJ <- aJ %>% left_join(Count19f1, by ='FID')
+aJ <- a %>% left_join(Count19, by ='id')
+aJ <- aJ %>% left_join(Count19T, by ='id')
+aJ <- aJ %>% left_join(Count19TH, by ='id')
+aJ <- aJ %>% left_join(Count19f1, by ='id')
 
 
 
@@ -132,39 +107,197 @@ plot(aJ['Tot19'])
 st_crs(aJ) = 4326
 
 ggplot() +
-  geom_sf(data = world) +
+  # geom_sf(data = world) +
   geom_sf(data= aJ,aes(fill=Tot19), alpha = 0.5)+
-  coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
+  # coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
   scale_fill_viridis_b() +
   ggtitle("GEDI Samples 2019")
-ggsave("GEDITot2019.png", width = 25, height = 18, units = "cm",dpi=300,path = 'GEDI_GLOBAL/figures' )
+ggsave("GEDITot2019.png", width = 25, height = 18, units = "cm",dpi=300,path = 'Figures' )
 
 
 
 
 plot(aJ['Trees'])
 ggplot() +
-  geom_sf(data = world) +
+  # geom_sf(data = world) +
   geom_sf(data= aJ,aes(fill=Trees), alpha = 0.5)+
-  coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
+  # coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
   scale_fill_viridis_b() +
   ggtitle("GEDI Trees 2019")
-ggsave("GEDITrees2019.png", width = 25, height = 18, units = "cm",dpi=300,path = 'GEDI_GLOBAL/figures' )
+ggsave("GEDITrees2019.png", width = 25, height = 18, units = "cm",dpi=300,path = 'Figures' )
 
 
 
-aJ <- aJ %>% mutate(Ratio19 = Trees/Tot19)%>%dplyr::filter(Tot19 > 50000)
+aJ <- aJ %>% mutate(Ratio19 = Trees/Tot19)%>%dplyr::filter(Tot19 > 10000)
 
 plot(aJ['Ratio19'])
 
 ggplot() +
-  geom_sf(data = world) +
-  geom_sf(data= aJ,aes(fill=Ratio19), alpha = 0.5)+
-  coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
-  scale_fill_viridis_b() +
+  # geom_sf(data = world) +
+  geom_sf(data= aJ,aes(fill=Ratio19*100), alpha = 0.5)+
+  # coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
+  scale_fill_viridis_c(limits=c(0,100)) +
   ggtitle("GEDI Ratio Tree vs Total 2019")
-ggsave("GEDIRatio2019.png", width = 25, height = 18, units = "cm",dpi=300,path = 'GEDI_GLOBAL/figures' )
+ggsave("GEDIRatio2019.png", width = 25, height = 18, units = "cm",dpi=300,path = 'Figures' )
+
+
+ggplot() +
+  # geom_sf(data = world) +
+  geom_sf(data= aJ|> dplyr:: filter(THM19>0),aes(fill=THM19), alpha = 0.5)+
+  # coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
+  scale_fill_viridis_c(limits=c(0,30)) +
+  ggtitle("GEDI Mean Canopy height 2019")
+ggsave("GEDI_CH_2019.png", width = 25, height = 18, units = "cm",dpi=300,path = 'Figures' )
 
 
 
 
+
+# ////////////////////////////////////
+
+# 2022
+
+Count22 <- readr::read_csv('Data//heights_2022_C.csv')%>% rename(Tot22=rh98 )
+Count22T <- readr::read_csv('Data/heights_2022_Cgt5.csv')%>% rename(Trees = rh98)
+Count22f <- readr::read_csv('Data/heights_2022_Cf.csv')%>% rename(Count22 = rh98)
+Count22TH <- readr::read_csv('Data/heights_2022_Median.csv')%>% rename(THM22 = rh98)
+
+Count22f$Count22 <- str_replace(Count22f$Count22, "\\{", "")
+Count22f$Count22 <- str_replace(Count22f$Count22, "\\}", "")
+Count22f$Count22 <- str_replace(Count22f$Count22, ".*_", "")
+
+
+Count22f1 <- Count22f %>%
+  mutate(rn = row_number()) %>%
+  # separate_rows(Count22, sep="=\\s*\\n*")  %>%
+  separate_rows(Count22, sep=",\\s*\\n*") %>%
+  mutate(Count22 = str_remove_all(Count22, '"')) %>%
+  separate(Count22, into = c("vars1", "vars2"), sep="\\s*=\\s*") %>% 
+  pivot_wider(names_from = vars1, values_from = vars2, 
+              values_fill = list(vars2 = '')) %>%
+  dplyr::select(-rn)%>%
+  # select(-'_count')%>%
+  dplyr::select(-'null')%>%
+  dplyr::select(-`system:index`)%>%
+  dplyr::select(-`.geo`)%>%
+  dplyr::select(-'0.0')%>%
+  dplyr::select(-left)%>%
+  dplyr::select(-right)%>%
+  dplyr::select(-bottom)%>%
+  dplyr::select(-top)%>%
+  
+  # rename('0' = `0.0`)%>%
+  rename('5' = `1.0`)%>%
+  rename('15' = `2.0`)%>%
+  rename('25' = `3.0`)%>%
+  rename('35' = `4.0`)%>%
+  rename('45' = `5.0`)%>%
+  rename('55' = `6.0`)%>%
+  rename('65' = `7.0`)%>%
+  rename('75' = `8.0`)%>%
+  rename('85' = `9.0`)
+
+
+Count22f1 <- Count22f1 %>% mutate_if(is.character,as.numeric)
+
+Count22f1Tot <- colSums(Count22f1[,2:10], na.rm =T)
+
+Count22f1Tot <- bind_rows(Count22f1Tot)
+
+Count22f1Tot_long <- Count22f1Tot %>%
+  pivot_longer(
+    c("5",  "15", "25", "55", "45", "35", "65", "75", "85"),
+    names_to = "H", values_to = "values"
+  )
+
+Count22f1Tot_long <- Count22f1Tot_long %>% mutate_if(is.character,as.numeric)
+Count22f1Tot_long2 <- Count22f1Tot_long  %>% mutate(v22 = values/ sum(Count22f1Tot_long$values, na.rm =T))
+
+p<-ggplot(data=Count22f1Tot_long, aes(x=H, y=values)) +
+  geom_bar(stat="identity")
+p
+p2<-ggplot(data=Count22f1Tot_long, aes(x=H, y=values)) +
+  # geom_bar(stat="identity")+
+  geom_density(stat="identity", fill=NA) 
+p2
+
+aJ22 <- a %>% left_join(Count22, by ='id')
+aJ22 <- aJ22 %>% left_join(Count22T, by ='id')
+aJ22 <- aJ22 %>% left_join(Count22TH, by ='id')
+aJ22 <- aJ22 %>% left_join(Count22f1, by ='id')
+
+
+
+plot(aJ22['Tot22'])
+st_crs(aJ22) = 4326
+
+ggplot() +
+  # geom_sf(data = world) +
+  geom_sf(data= aJ22,aes(fill=Tot22), alpha = 0.5)+
+  # coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
+  scale_fill_viridis_b() +
+  ggtitle("GEDI Samples 2022")
+ggsave("GEDITot2022.png", width = 25, height = 18, units = "cm",dpi=300,path = 'Figures' )
+
+
+
+
+plot(aJ22['Trees'])
+ggplot() +
+  # geom_sf(data = world) +
+  geom_sf(data= aJ22,aes(fill=Trees), alpha = 0.5)+
+  # coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
+  scale_fill_viridis_b() +
+  ggtitle("GEDI Trees 2022")
+ggsave("GEDITrees2022.png", width = 25, height = 18, units = "cm",dpi=300,path = 'Figures' )
+
+
+
+aJ22 <- aJ22 %>% mutate(Ratio22 = Trees/Tot22)%>%dplyr::filter(Tot22 > 10000)
+
+plot(aJ22['Ratio22'])
+
+ggplot() +
+  # geom_sf(data = world) +
+  geom_sf(data= aJ22,aes(fill=Ratio22*100), alpha = 0.5)+
+  # coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
+  scale_fill_viridis_c(limits=c(0,100)) +
+  ggtitle("GEDI Ratio Tree vs Total 2022")
+ggsave("GEDIRatio2022.png", width = 25, height = 18, units = "cm",dpi=300,path = 'Figures' )
+
+
+ggplot() +
+  # geom_sf(data = world) +
+  geom_sf(data= aJ22|> dplyr:: filter(THM22>0),aes(fill=THM22), alpha = 0.5)+
+  # coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
+  scale_fill_viridis_c(limits=c(0,30)) +
+  ggtitle("GEDI Mean Canopy height 2022")
+ggsave("GEDI_CH_2022.png", width = 25, height = 18, units = "cm",dpi=300,path = 'Figures' )
+
+# differences in TH
+
+aJTot <- aJ|> st_drop_geometry() |> left_join(aJ22|> st_drop_geometry() |> dplyr::select(id,THM22,Ratio22 ), by = 'id')
+aJTot <-  aJTot |> left_join(aJ |> dplyr::select(id), by = 'id')
+
+aJTot <- aJTot |> mutate(DeltaT = THM22 - THM19)
+aJTot <- aJTot |> mutate(DeltaArea = Ratio22 - Ratio19)
+
+aJTot <- st_as_sf(aJTot)
+
+ggplot() +
+  # geom_sf(data = world) +
+  geom_sf(data= aJTot,aes(fill=DeltaT), alpha = 0.5)+
+  # coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
+  scale_fill_gradient2(limits=c(-2, 2), oob=squish) +
+  ggtitle("GEDI Delta Canopy height 2022 2019")
+ggsave("GEDI_CH_Delta22_19.png", width = 25, height = 18, units = "cm",dpi=300,path = 'Figures' )
+
+
+
+ggplot() +
+  # geom_sf(data = world) +
+  geom_sf(data= aJTot,aes(fill=DeltaArea*100), alpha = 0.5)+
+  # coord_sf( ylim = c(-52, 53),xlim = c(-135, 160))+
+  scale_fill_gradient2(limits=c(-10, 10), oob=squish) +
+  ggtitle("GEDI Delta ForestCover 2022 2019")
+ggsave("GEDI_CH_DeltaArea22_19.png", width = 25, height = 18, units = "cm",dpi=300,path = 'Figures' )
